13eed5f2a22513e56b0e16de64b1126724c0fd57
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-10 08:22:48 -0300

    Soma de numeros

--- main.go
+++ main.go
@@ -1,59 +1,10 @@
 package main 
-
-import (
-    "fmt"
-    //"strings"
-    "sort"
-)
-
-func main () {
-    /*   greeting := "Hello my friends!"
-    fmt.Println(strings.Contains(greeting, "friends"))
-    fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-    fmt.Println(strings.ToUpper(greeting))
-    fmt.Println(strings.Index(greeting, "my"))
-    fmt.Println(strings.Split(greeting, "!"))    */
-    
-    ages := []int {50, 80, 10}
-    sort.Ints(ages)
-    fmt.Println(ages)
-    index := sort.SearchInts(ages, 50)
-    fmt.Println(index)
-    names := []string{"Pedro", "Bruno", "Yan"}
-    sort.Strings(names)
-    fmt.Println(names)
-    fmt.Println(sort.SearchStrings(names, "Pedro"))  
-    
-    
-   /* x := 0
-    for x <= 5 {
-        fmt.Println(x)
-        x++
-    }
-
-    for i := 0; i < 5; i++ {
-        fmt.Println("for 2: ", i)
-    }
-
-    for i := 0; i < len(names); i++ {
-        fmt.Println(names[1])
-    }
-
-    for index, value := range names {
-        fmt.Println("O indice é: ", index, "e o valor ", value)
-    } */
-
-    for index, value := range ages {
-        fmt.Println("O indice é:", index, "E o valor é: ", value)
-    }
-
-    superherois := []string{"DeadPool", "Homem Aranha", "Batman"}
-
-    /*  for index, value := range superherois {
-        fmt.Println("O número do herói é: ", index, "O nome do herói é: ", value)
-    }   */
-
-    for i:=0; i < len(superherois); i++ {
-        fmt.Println("O número do herói é:", i, "e o herói é o ", superherois[1])
-    }
-}
+  
+  import "fmt"
+ 
+  func main () {
+ 	var numeros = [5]int{}
+ 	fmt.Println("Digite 5 numeros")
+    fmt.Scan(&numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4])
+    fmt.Println("A soma dos numeros é: ", numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4] )
+  }
