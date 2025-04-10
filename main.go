1447fa3505165d06f3820c1e816daa8122423c71
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-10 09:37:39 -0300

    Condicional

--- main.go
+++ main.go
@@ -3,8 +3,31 @@ package main
   import "fmt"
  
   func main () {
- 	var numeros = [5]int{}
- 	fmt.Println("Digite 5 numeros")
-    fmt.Scan(&numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4])
-    fmt.Println("A soma dos numeros é: ", numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4] )
+ 	/*ages := 16
+    fmt.Println(ages <= 50)
+    fmt.Println(ages >= 50)
+    fmt.Println(ages == 50)
+    fmt.Println(ages != 50)
+
+    if ages < 15 {
+        fmt.Println("Menor que 15 anos")
+    } else if ages < 40 {
+        fmt.Println("Menor que 40 anos")
+    } else {
+        fmt.Println("Não é menor que 40 anos")
+    } */
+
+    names := []string{"Isadora", "Yasmim", "Trunks", "Cebolinha", "Martin", "Yuri"}
+
+    for index, value := range names {
+        if index == 1 {
+            fmt.Println("Continue após a posição", index, "e valor", value)
+            continue
+        }
+        if index > 2 {
+            fmt.Println("Sair após", index)
+        }
+
+        fmt.Println("Valor", value)
+    }
   }
