c017e40611bdf8ff126729ebd3b1b2d3bfb2f191
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-11 14:19:00 -0300

    Saques e depositos5

--- main.go
+++ main.go
@@ -1,56 +1,73 @@
 package main 
   
   import "fmt"
+
+  var valorDEP float64
+  var valorSAC float64
+  var ValorSaldo float64
+  var decisão float64
+  var Decisão2 float64
  
-  func main () {
- 	var saldo float64
-    var decisão float64
-    var sacado float64
-    var depositado float64
-    var decisão2 float64
-    fmt.Println("Digite seu saldo")
-    fmt.Scan(&saldo)
-    if saldo < 0 {
-        fmt.Println("Valor invalido")
-        main()
-    } else {
-    fmt.Println("Digite 1 para Sacar e 2 para Depositar")
+  func saudar() {
+    fmt.Println("Olá, tudo bem? O que você deseja fazer?")
+    fmt.Println("Qual função você deseja realizar:\n1-Depositar\n2-Sacar\n3-Ver saldo")
     fmt.Scan(&decisão)
-    if decisão == 1 {
-        fmt.Println("Qual valor você decide sacar?")
-        fmt.Scan(&sacado)
-        if sacado > saldo || sacado < 0 {
-            fmt.Println("Valor indisponivel  ")
-            main()
-        } else {
-            fmt.Println("Seu novo saldo é ", saldo - sacado)
-            fmt.Println("Você deseja executar uma nova operação? SIM - 1 Não - 2")
-            fmt.Scan(&decisão2)
-            if decisão2 == 1 {
-                main()
-            } else {
-                fmt.Println("Você finalizou sua operação")
-            }
-        }
+  }
+  
+  func sacar(){
+    fmt.Println("Qual valor você deseja sacar?")
+    fmt.Scan(&valorSAC) 
+    if valorSAC < 0 || valorSAC > ValorSaldo {
+        fmt.Println("Valor impossivel de ser sacado")
     } else {
-        if decisão == 2 {
-        fmt.Println("Qual valor você deseja depositar?")
-        fmt.Scan(&depositado)
-        if depositado < 0 {
-            fmt.Println("Impossivel adicionar valores menores que 0")
-            main()
-        } else {
-            fmt.Println("Seu novo saldo é", saldo + depositado)
-            fmt.Println("Você deseja executar uma nova operação? SIM - 1 Não - 2")
-            fmt.Scan(&decisão2)
-            if decisão2 == 1 {
-                main()
-            } else {
-                fmt.Println("Você finalizou sua operação")
-            }
-        }
+        ValorSaldo = ValorSaldo - valorSAC
+        fmt.Println("Seu novo saldo é ", ValorSaldo)
+    } 
+  }
+ 
+  func depositar() {
+    fmt.Println("Qual valor você deseja depositar?")
+    fmt.Scan(&valorDEP)
+    if valorDEP <= 0 {
+        fmt.Println("Impossivel depositar numeros menores ou igual a 0")
     } else {
-        fmt.Println("Error")
-        main()
+        ValorSaldo = ValorSaldo + valorDEP
+        fmt.Println("Seu novo saldo é", ValorSaldo)
     }
-  }  } }
+  }
+  
+  func saldo() {
+    fmt.Println("Seu saldo é ", ValorSaldo)
+  }
+  
+  func Action() {
+    if decisão == 1 {
+        depositar()
+    } else if decisão == 2 {
+        sacar()
+    } else if decisão == 3{
+        saldo()
+    } else {
+        fmt.Println("Ação inexistente")
+    }
+  }
+
+
+  func main () {
+    fmt.Println("Qual é o seu saldo inicial?")
+    fmt.Scan(&ValorSaldo)
+    saudar()
+    Action()
+    for {
+        fmt.Println("Você deseja realizar uma nova operação?\n1-Sim e 2-Não")
+        fmt.Scan(&Decisão2)
+        if Decisão2 == 2 {
+            break
+        } else if Decisão2 == 1{
+            saudar()
+            Action()
+        } else {
+            fmt.Println("Valor invalido")
+        }
+    }
+  }
