e09537f3e4485dcc8bf37e9de87ce4fb2292de39
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-17 09:27:17 -0300

    Idades

--- main.go
+++ main.go
@@ -1,35 +1,29 @@
 package main 
-  
-  import (
-    "fmt"
-  )
+    
+    import (
+      "fmt"
+    )
 
-  func dividir(dividendo int, divisor int) (int, string){
-    if divisor == 0 {
-      return 0, "Erro na divisão por zero"
+    func infoPessoais(idade int, nome string) (int, string){
+      if idade <= 0  || idade > 110 {
+        fmt.Println("Idade invalida")
+      } else if idade < 18 || idade > 0 {
+        fmt.Println("Você é menor de idada") 
+      } else {
+        fmt.Println("Você é maior de idade")
+      }
+      return idade, nome
     }
-    resultado := dividendo / divisor
-    return resultado, "Sem erro"
-  }
-
-  func operacaoBasica(a int, b int) (int, int, int) {
-    soma := a + b 
-    multiplicacao := a * b 
-    subtracao := a - b 
-    return soma, multiplicacao, subtracao
-  }
-  
-  func main () {
-    resultado, erro := dividir(10, 2)
-
-    if erro != "Sem erro" {
-      fmt.Println(erro)
-    } else {
-      fmt.Println("O resultado da divisão é:", resultado)
+    
+    func main () {
+      var idade int
+      var nome string
+      fmt.Println("Qual a sua idade?")
+      fmt.Scan(&idade)
+      fmt.Println("Qual o seu nome?")
+      fmt.Scan(&nome)
+      
+      fmt.Println("Nome: ", nome)
+      fmt.Println("Idade: ", idade)
+      infoPessoais(idade, nome)
     }
-
-    soma, mult, sub := operacaoBasica(10,2) 
-    fmt.Println(soma)
-    fmt.Println(mult)
-    fmt.Println(sub)
-   }
