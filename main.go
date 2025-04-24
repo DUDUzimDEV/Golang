d3f92117887e3a3d72e9ab9a94f97b7cd7946f1e
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-24 14:54:20 -0300

    Estoque

--- main.go
+++ main.go
@@ -4,26 +4,24 @@ package main
       "fmt"
     )
 
-    var media float64
-    var nota1 float64
-    var nota2 float64
-
-    func analisarNotas() (float64, string) {
-      fmt.Println("Quais são as notas?")
-      fmt.Scan(&nota1)
-      fmt.Scan(&nota2)
-      media := (nota1 + nota2) / 2
-      if media >= 6 {
-        return media, "Aprovado"
-      } else if media <= 5 || media > 0 { 
-        return media, "Reprovado"
-      } else {
-        return media, "Erro"
-      }
-    }
-    
     func main () {
-      media, resultado := analisarNotas()
-      fmt.Println(media)
-      fmt.Println(resultado)
+      inventario := map[string]int {
+        "COXINHA": 10,
+        "PÃO DE QUEIJO": 15,
+        "REFRIGERANTE": 20,
+      }
+
+      for K, V := range inventario {
+        fmt.Println("Item:", K, "Quantidade:", V)
+      }
+      
+      fmt.Println("------------------------------------------------------")
+
+      inventario["COXINHA"] -= 2
+
+      inventario["PÃO DE QUEIJO"] -= 1
+
+      for K, V := range inventario {
+        fmt.Println("Item:", K, "Quantidade:", V)
+      }
     }
